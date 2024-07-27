00ff502c0b32dc1f40ead9f16ff493ee64c3ebd1
Raul <168528192+Raulisr00t@users.noreply.github.com>
2024-07-27 12:23:12 +0400

    Add files via upload

---
+++ phishing.py
@@ -0,0 +1,51 @@
+import requests
+import smtplib
+import time
+import asyncio
+import random
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+users_list = [] 
+files = ["pmail1.html", "pmail2.html", "pmail3.html", "pmail4.html", "pmail5.html", 
+         "pmail6.html", "pmail7.html", "pmail8.html", "pmail9.html", "pmail10.html"]
+
+user = input("Please enter a UserMail:")
+file_input = input("Please enter a filename for phishing:")
+
+port = 587
+smtp_server = "smtp.gmail.com"
+owner_mail = "pxmpkeen@gmail.com"
+owner_password = "eiaw fckm dqkd eaky"
+
+def send_mail(reciever_email):
+    context = ssl.create_default_context()
+    with open(file_input,"r") as f:
+        content = f.read()
+        message = MIMEMultipart("alternative")
+    message["From"] = owner_mail
+    message["To"] = reciever_email
+    message["Subject"] = "Your OTP Code"
+
+    #plain_text = f"Your OTP code is: {otp}\n\nUse the following link if needed: {activation_link}"
+    #message.attach(MIMEText(plain_text, "plain"))
+    message.attach(MIMEText(content, "html"))
+
+    try:
+        with smtplib.SMTP(smtp_server, port) as mail:
+            mail.ehlo()
+            mail.starttls(context=context)
+            mail.login(owner_mail, owner_password)
+            mail.sendmail(owner_mail, reciever_email, message.as_string())
+            print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    #receiver_email = input("Enter the recipient's email address: ")
+    send_mail(user)
+
+        #print(f"OTP sent to {receiver_email}: {otp}")
+
+#server = smtplib.SMTP()
